e50925bfdca254bbc9b65331561f8b90716336e3
Kaushik Lakshminarasimhan <jklakshm@bcm.edu>
2019-09-05 16:25:04 -0400

    cosine similarity

--- nancorr.m
+++ nancorr.m
@@ -1,4 +1,4 @@
-function [r,p] = nancorr(x,y)
+function [r,p] = nancorr(x,y,varargin)
 
 % correlation between two vectors with nans
-[r,p] = corr(x(~isnan(x) & ~isnan(y)),y(~isnan(x) & ~isnan(y)));
+[r,p] = corr(x(~isnan(x) & ~isnan(y)),y(~isnan(x) & ~isnan(y)), varargin{:});
